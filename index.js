44eaae321aca20c779b7ace44a273e3e1130dba9
chaitu9876 <20211a05r6@bvrit.ac.in>
2022-11-28 11:55:22 +0530

    project changed

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-function hello()
-{
-    console.log("Hello Node.js!");
-}
-hello();  
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent="";
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+fs.readFile("registration.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = project;
+});
+let args = require("minimist")(process.argv.slice(2), {
+  alias: {
+    port: "PORT",
+  },
+ });
+ const port=args.port;
+ http .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+         response.write(registrationContent);
+         response.end();
+         break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(`${port}`);
+ 
